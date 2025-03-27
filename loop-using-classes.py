f90de25c8f88efeffa7be88abcdf9d2d6f9f82f4
Biraz Thapa <78890253+poodypai@users.noreply.github.com>
2025-03-27 12:55:06 +0545

    Update loop-using-classes.py

--- loop-using-classes.py
+++ loop-using-classes.py
@@ -1,10 +1,12 @@
 class loopy:
+    @classmethod
     def input_1(cls):
         nm = int(input("How long should the loop go for? "))
         nm2 = input("What should it print? ")
         return nm, nm2
 
 class Act1(loopy):
+    @classmethod
     def myfunc(cls, inp, inp1):
         x = inp
         for j in range(x):
@@ -13,6 +15,6 @@ class Act1(loopy):
 def main():
     nm, nm2 = loopy.input_1()
     Act1.myfunc(nm, nm2)
-
- 
-main()
+    
+if __name__ == "__main__":
+    main()
